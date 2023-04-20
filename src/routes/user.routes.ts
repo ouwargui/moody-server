faa9cb6b2f9938fedc58a59a1a0c3a5fcf47046b
ouwargui <guikemon2@gmail.com>
2023-04-20 01:35:43 -0300

    feat: create routes for the created usecases

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,13 @@
 import {Router} from 'express';
 import {Controller} from '../protocols/controller';
-import {UserController} from '../controllers/user-controller';
+import {CreateOrGetUserUseCase} from '../usecases/user/CreateOrGetUserUseCase';
+import {CreateUserProfileUseCase} from '../usecases/user/CreateUserProfileUseCase';
+import {GetUserProfileUseCase} from '../usecases/user/GetUserProfileUseCase';
 
 const userRoutes = Router();
 
-userRoutes.get('/', Controller(UserController));
+userRoutes.get('/', Controller(CreateOrGetUserUseCase));
+userRoutes.get('/profile', Controller(GetUserProfileUseCase));
+userRoutes.post('/profile', Controller(CreateUserProfileUseCase));
 
 export {userRoutes};
